e9ed4bfea28fceeb3efccc1f5de72e271ae4c864
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-08 20:41:54 +0100

    Add initial setup for movie app: API requests, main components, and routing
    Established foundational components including API request setup, main components for rendering movies, and basic routing structure for navigation within the movie app.

---
+++ src/Request.js
@@ -0,0 +1,11 @@
+const key = "1a69d2ab98c693b4e5bf73b520d558d6";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+function Main() {
+  const [movies, setMovies] = useState([]);
+  return <div>Main</div>;
+}
+
+export default Main;

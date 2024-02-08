98c98ec166ef3292b29afbb7e2dbc862d42bc0fe
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-08 21:01:07 +0100

    Implement random movie backdrop display in Main component
    Added functionality to display a random movie backdrop image fetched from the API in the Main component.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,12 +4,24 @@ import axios from "axios";
 
 function Main() {
   const [movies, setMovies] = useState([]);
+  const movie = movies[Math.floor(Math.random() * movies.length)];
   useEffect(() => {
     axios.get(requests.requestPopular).then((response) => {
-      setMovies(response.data);
+      setMovies(response.data.results);
     });
   }, []);
-  return <div>Main</div>;
+  return (
+    <div className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        <img
+          className="w-full h-full object-cover"
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.title}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Main;

52a976e0e87e49ea5ac2b2110d836343f5a05c56
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-08 20:48:52 +0100

    Implement API data fetching and rendering of popular movies in Main component and integrate Main component into Home page
    Add movie data fetching to Main component & integrate it into Home page. Fetching popular movies using Axios, then updating the state accordingly. The Home page now displays the Main component, serving as the central hub for movie display.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import requests from "../Request";
+import axios from "axios";
 
 function Main() {
   const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((response) => {
+      setMovies(response.data);
+    });
+  }, []);
   return <div>Main</div>;
 }
 

943bd996521cb909c7ffa147a22b1647990ef166
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-10 00:21:54 +0100

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,6 +19,16 @@ function Main() {
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
+        <div>
+          <div className="absolute w-full top-[20%] p-4 md:p-8">
+            <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
+              Play
+            </button>
+            <button className="border text-white border-gray-300 py-2 px-5 ml-4">
+              Watch Later
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );

df35e9e3a1da97c0bbd25316c137b4d8e303bfa4
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-11 09:36:56 +0100

    Improve readability and maintainability by extracting helper functions and optimizing JSX structure

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,6 +10,13 @@ function Main() {
       setMovies(response.data.results);
     });
   }, []);
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
   return (
     <div className="w-full h-[550px] text-white">
       <div className="w-full h-full">
@@ -19,8 +26,9 @@ function Main() {
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
-        <div>
-          <div className="absolute w-full top-[20%] p-4 md:p-8">
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <div className="my-4">
             <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
               Play
             </button>
@@ -28,6 +36,12 @@ function Main() {
               Watch Later
             </button>
           </div>
+          <p className="text-gray-400 text-sm">
+            <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+              {truncateString(movie?.overview, 150)}
+            </p>
+            Released: {movie?.release_date}
+          </p>
         </div>
       </div>
     </div>

7ecc7c888c63b14f94c5e21e51d435f66140eb8f
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-11 15:47:24 +0100

    Create Account Login and Signup Pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+
 function App() {
   return (
     <>
@@ -9,6 +13,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>

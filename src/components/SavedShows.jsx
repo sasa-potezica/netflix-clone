aef1692cce151526f8b7bce439558a8981fa40b2
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-12 14:02:08 +0100

    Implement functionality to delete saved shows and Add Favicon and Site Title

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -3,6 +3,7 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { UserAuth } from "../context/AuthContext";
 import { db } from "../firebase";
 import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SavedShows = () => {
   const [movies, setMovies] = useState([]);
@@ -14,6 +15,25 @@ const SavedShows = () => {
   const slideRight = () => {
     var slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  useEffect(() => {
+    onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
+      setMovies(doc.data()?.savedShows);
+    });
+  }, [user?.email]);
+
+  const movieRef = doc(db, "users", `${user?.email}`);
+
+  const deleteShow = async (passedID) => {
+    try {
+      const result = movies.filter((item) => item.id !== passedID);
+      await updateDoc(movieRef, {
+        savedShows: result,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -31,7 +51,10 @@ const SavedShows = () => {
         >
           {movies.map((item, id) => {
             return (
-              <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
+              <div
+                key={id}
+                className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
+              >
                 <img
                   className="w-full h-auto block"
                   src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
@@ -40,6 +63,12 @@ const SavedShows = () => {
                 <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
                   <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
                     {item?.title}
+                  </p>
+                  <p
+                    onClick={() => deleteShow(item.id)}
+                    className="absolute text-gray-300 top-4 right-4"
+                  >
+                    <AiOutlineClose />
                   </p>
                 </div>
               </div>

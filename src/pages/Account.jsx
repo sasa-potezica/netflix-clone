b1aa12d2d222ad853e93ea871a6400d87082daea
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-12 12:29:37 +0100

    Implement login and signup functionality Implement authentication functionality using Firebase

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://miro.medium.com/v2/resize:fit:1400/1*5lyavS59mazOFnb55Z6znQ.png"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Account;
